19dd9fd2b2172256bb398cc6e7359f6c94d2963a
syedwaqas2292 <92338766+syedwaqas2292@users.noreply.github.com>
2023-09-25 16:11:26 +0000

    Conversion of data types

---
+++ 01_basics/02_basics_ConversionType.js
@@ -0,0 +1,31 @@
+let score = "Waqas";
+
+
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false:0;
+
+
+let isLoggedIn = "waqas";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+
+// 1 => true ; 0 => false
+// ""=> false ; "waqas" => true;
+
+
+
+let SomeNumber = 35;
+
+let StringNumber = String(SomeNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
