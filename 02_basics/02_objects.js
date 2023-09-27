ce74413dbf7e2b8c178fa29b79c3cd36aa56a20c
syedwaqas2292 <92338766+syedwaqas2292@users.noreply.github.com>
2023-09-27 19:17:12 +0000

    Objects

---
+++ 02_basics/02_objects.js
@@ -0,0 +1,21 @@
+// Singleton
+// Singleton Literals se declare krnay p ni bnta 
+// Singleton Constructors se bnyga to hamesha singleton bnayga
+// Object.create singleton constructor 
+
+
+//object literals
+const Jsuser = {
+    name: "waqas",
+    "full name":"syed waqas",
+    age: 25,
+    location: "Karachi",
+    email: "xyz@gamil.com",
+    isLoggedIn: false,
+    lastLoginDays:["Monday","Saturday"]
+}
+
+console.log(Jsuser.email);
+console.log(Jsuser["email"]);
+console.log(Jsuser["full name"]);
+
