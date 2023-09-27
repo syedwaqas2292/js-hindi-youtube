c9d02090a9924e886cc62b6ec732dda75c187216
syedwaqas2292 <92338766+syedwaqas2292@users.noreply.github.com>
2023-09-27 19:44:00 +0000

    Objects

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -5,9 +5,13 @@
 
 
 //object literals
+
+const mySymb = Symbol("Key1")  // using Symbol in object example
+
 const Jsuser = {
     name: "waqas",
     "full name":"syed waqas",
+    [mySymb]:"mykey1", // using Symbol in object example symbol ko refrence krna ho to [] brakets use hotay hain
     age: 25,
     location: "Karachi",
     email: "xyz@gamil.com",
@@ -15,7 +19,23 @@ const Jsuser = {
     lastLoginDays:["Monday","Saturday"]
 }
 
-console.log(Jsuser.email);
-console.log(Jsuser["email"]);
-console.log(Jsuser["full name"]);
+// console.log(Jsuser.email);
+// console.log(Jsuser["email"]);
+// console.log(Jsuser["full name"]);
+// console.log(Jsuser[mySymb]); // Call Symbol in object example
 
+
+// Jsuser.email = "waqas@gmail.com";
+// Object.freeze(Jsuser) // Dont Change Freeze Values.
+// Jsuser.email = "waqas@chatgpt.com";
+// console.log(Jsuser);
+
+Jsuser.greetings = function () {
+    console.log("Hellow Js User");
+}
+Jsuser.greetingstwo = function () {
+    console.log(`Hellow Js User, ${this.name}`); //using string interpolation and this using object ki values ko reference krnay kay liye use krtay hain.
+}
+
+console.log(Jsuser.greetings()); // Function Anoymous function ka refrence aya hai Execute ni huwa hai 
+console.log(Jsuser.greetingstwo());
