f0fee252cc225a6ca0cd9bb8d6787b56ecd1b3f4
syedwaqas2292 <92338766+syedwaqas2292@users.noreply.github.com>
2023-10-02 17:39:17 +0000

    Obects2

---
+++ 02_basics/03_objects2.js
@@ -0,0 +1,14 @@
+//const tinderuser = new Object ()  //Singleton Object hai 
+const tinderuser1 = {}; // Non Singleton Object
+
+
+tinderuser1.id = "123456abc";
+tinderuser1.name = "Sam"
+tinderuser1.isLoggedIn = false
+
+
+// console.log(tinderuser1);
+
+
+
+
